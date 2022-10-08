531513980cd75ec458dbb50c160a7f01ce6a0d1f
Oleg <tmr777@mail.ru>
2022-10-08 20:31:24 +0300

    Домашнее задание. Задача 15

---
+++ DZ3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа определяет  по номеру день недели и проверят является ли он выходным днем.
+
+Console.Write("Введите число (1 - 7), обозначающее день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void NumDay (int day) {
+  if (day == 6 || day == 7) {
+  Console.WriteLine("Этот день выходной? -> да");
+  }
+  else if (day < 1 || day > 7) {
+    Console.WriteLine("Введено не то число, смотри условие ввода");
+  }
+  else Console.WriteLine("(Этот день выходной? -> нет");
+}
+
+NumDay(day);
+
+

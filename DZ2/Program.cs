0d864d6ea5102855ee4f6f4458878d4fe15fb182
Oleg <tmr777@mail.ru>
2022-10-08 15:40:35 +0300

    Домашнее задание. Задача 13

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,16 +1,44 @@
 ﻿//Программа выводит третью цифру выданного числа, или объявляет что ее нет.
-//int num = new Random().Next(100000);
+//
 
 Console.WriteLine("Введите число:"); 
-int num = int.Parse(Console.ReadLine()!);
+int num = new Random().Next(100000);
 
-if(num < 100){
-int num1 = num;
+//Console.WriteLine("Введите число:"); 
+//int num = int.Parse(Console.ReadLine()!);
+
+int num1;
+
+
+if (num > 0 && num < 100){
+
 Console.WriteLine($"{num} => Третьей цифры нет");
 }
 
-if(num < 1000){
-int num2 = num % 10;
+if (num > 99 && num < 1000){
+num1 = num % 10;
+Console.WriteLine($"{num} => {num1}");
+}
+
+ 
+if (num>999 && num < 10000){
+num1 = num /10 % 10;
+Console.WriteLine($"{num} => {num1}");
+}
+
+else{
+num1 = num /100 % 10;
+Console.WriteLine($"{num} => {num1}");
+}
+
+
+
+
+/*Console.WriteLine($"{num} Третьего числа нет");
+}
+
+if(num4 < 1000){
+int num2 = (num4 / 1 % 10);
 Console.WriteLine($"{num} => {num2}");
 }
 
@@ -22,5 +50,5 @@ Console.WriteLine($"{num} => {num3}");
 else{
 int num3 = num / 100 % 10;
 Console.WriteLine($"{num} => {num3}");
-}
+}*/
 

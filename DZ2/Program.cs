10e62070bc516173eb188ded2eaebfccd28453f1
Oleg <tmr777@mail.ru>
2022-10-08 21:03:13 +0300

    Домашние задание. Задача 15

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,17 +1,14 @@
 ﻿//Программа выводит третью цифру выданного числа, или объявляет что ее нет.
 //
 
-Console.WriteLine("Введите число:"); 
-int num = new Random().Next(100000);
-
 //Console.WriteLine("Введите число:"); 
-//int num = int.Parse(Console.ReadLine()!);
+//int num = new Random().Next(100000);
 
-int num1;
-
-
-if (num > 0 && num < 100){
-
+Console.WriteLine("Введите число:"); 
+int num = int.Parse(Console.ReadLine()!);
+int num1 = num;
+void ChisloTree (int num){
+if (num > 1 && num < 100){
 Console.WriteLine($"{num} => Третьей цифры нет");
 }
 
@@ -20,16 +17,20 @@ num1 = num % 10;
 Console.WriteLine($"{num} => {num1}");
 }
 
- 
 if (num>999 && num < 10000){
 num1 = num /10 % 10;
 Console.WriteLine($"{num} => {num1}");
 }
-
 else{
 num1 = num /100 % 10;
 Console.WriteLine($"{num} => {num1}");
 }
+}
+
+ ChisloTree (num);
+
+
+
 
 
 
